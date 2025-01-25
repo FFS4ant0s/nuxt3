4e4a209574b4370d76b19d1564f376f71d0b786a
Fernando <silentguyband@gmail.com>
2025-01-25 15:39:34 -0300

    Composables

--- components/VideoDetalhes.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Exemplo</h1>
-</template>
--- components/video/Detalhes.vue
+++ components/video/Detalhes.vue
@@ -1,4 +1,14 @@
+<script setup lang="ts">
+const {diasPassados, diferencaEmDias} = useFormatacao()
+
+const dataVideo = new Date(2024, 1, 1);
+
+diasPassados(dataVideo);
+
+</script>
+
 <template>
     <h1>Componenetes - Nuxt 3</h1>
     <p>Autor: Fernando Fernandes</p>
+    <p>{{ diferencaEmDias }} dias atrás</p>
 </template>
